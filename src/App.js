d270b5890f8db5a9b2af57676a72813a64e2f38c
Jhonny Neciosup <jhonny.millones@gmail.com>
2019-12-24 00:04:38 -0500

    edit (MainLayout): Include Header and Sidebar components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import MainLayout from './layouts/MainLayout';
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#F5F5F5' },
+    secondary: { main: '#cddc39' }
+  }
+});
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainLayout/>
     </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/layouts/MainLayout.js
@@ -0,0 +1,58 @@
+import React, { Fragment } from "react";
+import clsx from "clsx";
+import { makeStyles } from '@material-ui/core/styles';
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  content: {
+    flexGrow: 1,
+    marginLeft: theme.spacing.unit * 9,
+    padding: theme.spacing.unit * 3,
+    marginTop: theme.spacing.unit * 7,
+    overflowX: "hidden"
+  },
+  contentShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  }
+}));
+
+const  MainLayout = ( props) => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleToggleDrawer = () => {
+    this.setState(prevState => {
+      return { open: !prevState.open };
+    });
+  };
+
+    return (
+      <Fragment>
+        <div>
+          <Header
+            handleToggleDrawer={handleToggleDrawer}
+          />
+          <main
+            className={clsx(classes.content,classes.contentShift)}
+          >
+            {props.children}
+          </main>
+        </div>
+        <Sidebar open={open} drawerWidth={drawerWidth} />
+      </Fragment>
+    );
+  
+}
+
+export default MainLayout;
