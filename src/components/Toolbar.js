6a6b84b67a250867f193806cddee3f5b5c9aba63
Jhonny Neciosup Millones <jhonny.neciosup@indagia.com>
2019-12-27 04:27:51 -0500

    refactor(Toolbar): Update styles

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -5,16 +5,30 @@ import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import ProgressQuestion from './ProgressQuestion';
+import Link from '@material-ui/core/Link';
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    alignItems: 'center'
+    alignItems: 'center',
+    height: theme.spacing(12)
   },
   back: {
     display: 'flex',
+    width: theme.spacing(12),
     flexDirection: 'column',
     alignItems: 'center',
-    padding: '8px'
+    padding: theme.spacing(1)
+  },
+  title: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    padding: theme.spacing(2)
+  },
+  centerVertical: {
+    display: 'flex',
+    alignItems: 'center'
   }
 }));
 
@@ -26,26 +40,28 @@ const Toolbar = () => {
     'BCP - Sub Gerente de Ingeniería de Defensa de Ciberseguridad.';
   return (
     <Grid container className={classes.root} alignContent="space-between">
-      <Grid item xs={8}>
+      <Grid item xs={10}>
         <Grid container>
-          <Grid>
-            <div className={classes.back}>
-              <ArrowBackIcon />
-              <Typography>Atrás</Typography>
-            </div>
+          <Grid className={classes.centerVertical}>
+            <Link>
+              <div className={classes.back}>
+                <ArrowBackIcon />
+                <Typography>Atrás</Typography>
+              </div>
+            </Link>
           </Grid>
           <Grid>
             <Divider orientation="vertical" />
           </Grid>
-          <Grid>
-            <div className={classes.back}>
+          <Grid className={classes.centerVertical}>
+            <div className={classes.title}>
               <Typography variant="h6">{props.title}</Typography>
               <Typography variant="subtitle1">{props.subTitle}</Typography>
             </div>
           </Grid>
         </Grid>
       </Grid>
-      <Grid item xs={4}>
+      <Grid item xs={2}>
         <ProgressQuestion total={5} avance={1} />
       </Grid>
     </Grid>
