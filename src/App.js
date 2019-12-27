d15baa4d66eb9b209f1a25e18052c7b6df3f8572
Jhonny Neciosup Millones <jhonny.neciosup@indagia.com>
2019-12-27 04:32:16 -0500

    refactor(App): Add CssBaseline

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 import MainLayout from './layouts/MainLayout';
+import CssBaseline from '@material-ui/core/CssBaseline';
 const theme = createMuiTheme({
   palette: {
-    primary: { main: '#F5F5F5' },
+    primary: { main: '#2196f3' },
     secondary: { main: '#cddc39' }
   }
 });
@@ -12,6 +13,7 @@ const theme = createMuiTheme({
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <div className="App">
         <MainLayout />
       </div>
